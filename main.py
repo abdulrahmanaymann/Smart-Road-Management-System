8f6d54861138d8ecf0e1c72f4c0dfb17a73b7124
Abdulrahman Ayman <abdelrhmanayman002@gmail.com>
2023-10-29 19:01:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import redis
 from confluent_kafka import Producer
 
 
-from GUI import display_data
+from GUI import *
 from data_handler import *
 from config import *
-from excel_reader import read_excel_sheets
-from kafka_consumer import kafka_consumer
+from excel_reader import *
+from kafka_consumer import *
 
 
 def main():
@@ -18,15 +18,19 @@ def main():
 
     governorates_dict = insert_governorates_data(r, df_governorates)
     insert_vehicles_data(r, df_vehicles)
-    process_travels_data(df_travels, governorates_dict, r, producer)
-
-    r.close()
 
     # Start the Kafka consumer in a separate process
     kafka_consumer_process = multiprocessing.Process(target=kafka_consumer)
     kafka_consumer_process.start()
 
-    display_data()
+    # Start the GUI
+    df_travels = add_travel_record(df_travels)
+
+    # Process the updated data, including the new records
+    process_travels_data(df_travels, governorates_dict, r, producer)
+
+    r.close()
+    producer.flush()
 
 
 if __name__ == "__main__":
