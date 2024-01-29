b7d44d6725f1b10f348dbfd8b5c88ef3c78f3a66
Abdulrahman Ayman <abdelrhmanayman002@gmail.com>
2024-01-29 19:38:58 +0200

    Delete excel_reader.py

--- excel_reader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from config import *
-
-
-def read_excel_sheets(file_path):
-    # Read all excel sheets into a dictionary
-    sheets = pd.read_excel(file_path, sheet_name=[SHEET1, SHEET2, SHEET3])
-    return sheets[SHEET1], sheets[SHEET2], sheets[SHEET3]
