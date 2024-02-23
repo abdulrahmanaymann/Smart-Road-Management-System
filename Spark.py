0376312d927d618757d0f875d56480a1445a7b33
Abdulrahman Ayman <abdelrhmanayman002@gmail.com>
2024-02-24 01:47:23 +0200

    c

--- Spark.py
+++ Spark.py
@@ -1,111 +1,128 @@
 import findspark
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-from pyspark.sql.functions import (
-    col,
-    split,
-    substring_index,
-    countDistinct,
-    trim,
-    regexp_replace,
-)
+import pyspark.sql.functions as F
+from pyspark.sql.functions import *
+from pyspark.sql.streaming import DataStreamReader
+from pyspark.sql.types import *
 import os
-import shutil
 from Config.Logger import *
 from Config.config import *
 
-try:
-    os.system("command " + STDERR_REDIRECT_COMMAND)
-    findspark.init()
-    findspark.find()
+# ----------------- Init and find Spark -----------------
+findspark.init()
+findspark.find()
 
-    # Set HADOOP_HOME to PATH
-    os.environ["HADOOP_HOME"] = HADOOP_PATH
-    os.environ["PATH"] += ";" + os.path.join(os.environ["HADOOP_HOME"], "bin")
+# ----------------- Create SparkSession -----------------
+spark = (
+    SparkSession.builder.appName("Spark")
+    .config("spark.streaming.stopGracefullyOnShutdown", True)
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
+    .config("spark.jars", MYSQL_CONNECTOR_PATH)
+    .config("spark.driver.extraClassPath", MYSQL_CONNECTOR_PATH)
+    .config("spark.sql.shuffle.partitions", 4)
+    .master("local[*]")
+    .getOrCreate()
+)
 
-    # Create SparkSession
-    spark = (
-        SparkSession.builder.appName("TestSparkSession")
-        .config("spark.jars", MYSQL_CONNECTOR_PATH)
-        .config("spark.driver.extraClassPath", MYSQL_CONNECTOR_PATH)
-        .config("spark.local.dir", TEMP_PATH)
-        .getOrCreate()
+# ----------------- Read travels table from MySQL -----------------
+travels_df = (
+    spark.read.format("jdbc")
+    .option("url", f"jdbc:mysql://{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
+    .option("dbtable", "travels")
+    .option("user", MYSQL_USER)
+    .option("password", MYSQL_PASSWORD)
+    .load()
+)
+# travels_df.show()
+
+# ----------------- Read violations table from MySQL -----------------
+violations_df = (
+    spark.read.format("jdbc")
+    .option("url", f"jdbc:mysql://{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
+    .option("dbtable", "violations")
+    .option("user", MYSQL_USER)
+    .option("password", MYSQL_PASSWORD)
+    .load()
+)
+
+# violations_df.show()
+
+# ----------------- Join the two tables -----------------
+joined_df = (
+    travels_df.join(violations_df, travels_df["ID"] == violations_df["Car_ID"], "inner")
+    .select(
+        violations_df["Car_ID"],
+        travels_df["Start_Gate"],
+        travels_df["End_Gate"],
+        violations_df["Start_Date"],
+        violations_df["End_Date"],
     )
+    .orderBy(violations_df["Start_Date"])
+)
 
-    spark.sparkContext.setLogLevel("WARN")
+# joined_df.show()
 
-    # Read travels table from MySQL as a streaming DataFrame
-    travels_df = (
-        spark.readStream.format("jdbc")
-        .option("url", f"jdbc:mysql://{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
-        .option("dbtable", "travels")
-        .option("user", MYSQL_USER)
-        .option("password", MYSQL_PASSWORD)
-        .load()
+# ----------------- Count the number of violations for each route -----------------
+route_violations = (
+    joined_df.groupBy("Start_Gate", "End_Gate")
+    .count()
+    .orderBy("count", ascending=False)
+)
+
+route_violations.show()
+
+# ----------------- Read Kafka Stream -----------------
+violations_stream_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", KAFKA_BROKER)
+    .option("subscribe", VIOLATIONS_TOPIC)
+    .option("startingOffsets", "latest")
+    .load()
+)
+
+# Consumer: {'ID': '70_Car-GIZ', 'Start Gate': 'Giza', 'End Gate': 'Qalyubia', 'Start Date': '2024-02-23 14:52:59', 'End Date': '2024-02-23 14:53:06'}
+violations_schema = StructType(
+    [
+        StructField("ID", StringType(), True),
+        StructField("Start Gate", StringType(), True),
+        StructField("End Gate", StringType(), True),
+        StructField("Start Date", StringType(), True),
+        StructField("End Date", StringType(), True),
+    ]
+)
+
+violations_stream_df = (
+    violations_stream_df.selectExpr("CAST(value AS STRING)")
+    .select(from_json("value", violations_schema).alias("data"))
+    .select("data.*")
+)
+
+# ----------------- Count the number of violations for each route and print start gate end gate count -----------------
+joined_df_stream = violations_stream_df.join(
+    route_violations,
+    (violations_stream_df["Start Gate"] == route_violations["Start_Gate"])
+    & (violations_stream_df["End Gate"] == route_violations["End_Gate"]),
+    "inner",
+).select(
+    violations_stream_df["Start Gate"],
+    violations_stream_df["End Gate"],
+    (route_violations["count"]).alias("Total Violations"),
+)
+
+# joined_df_stream.show()
+
+# ----------------- Write the result to the console and show message after each batch to tell me the most violated route -----------------
+query = (
+    joined_df_stream.writeStream.outputMode("update")
+    .format("console")
+    .option("truncate", "false")
+    .foreachBatch(
+        lambda df, epoch_id: print(
+            f"Batch: {epoch_id}, Most Violated Route: {df.orderBy('Total Violations', ascending=False).first()['Start Gate']} to {df.orderBy('Total Violations', ascending=False).first()['End Gate']} with {df.orderBy('Total Violations', ascending=False).first()['Total Violations']} violations"
+        )
     )
+    .start()
+)
 
-    # Print streaming travels DataFrame
-    travels_query = (
-        travels_df.writeStream.format("console").outputMode("complete").start()
-    )
-
-    # Read violations table from MySQL as a streaming DataFrame
-    violation_df = (
-        spark.readStream.format("jdbc")
-        .option("url", f"jdbc:mysql://{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
-        .option("dbtable", "violations")
-        .option("user", MYSQL_USER)
-        .option("password", MYSQL_PASSWORD)
-        .load()
-    )
-
-    # Print streaming violation DataFrame
-    violation_query = (
-        violation_df.writeStream.format("console").outputMode("complete").start()
-    )
-
-    # ----------------- 1. Find the number of violations for each vehicle -----------------
-    violations_count_df = violation_df.groupBy("Car_ID").agg(
-        F.count("Car_ID").alias("Violations_Count")
-    )
-
-    # Print streaming violations count DataFrame
-    violations_count_query = (
-        violations_count_df.writeStream.format("console").outputMode("complete").start()
-    )
-
-    # ----------------- 2. Find the number of violations for each vehicle type -----------------
-    violations_count_df = violation_df.withColumn(
-        "Vehicle_Type", split(substring_index(col("Car_ID"), "_", -1), "-")[0]
-    )
-    violations_count_df = violations_count_df.groupBy("Vehicle_Type").agg(
-        F.count("Car_ID").alias("Violations_Count")
-    )
-
-    # Print streaming violations count by vehicle type DataFrame
-    violations_count_by_type_query = (
-        violations_count_df.writeStream.format("console").outputMode("complete").start()
-    )
-
-    # ----------------- 3. Find the number of each start and end gate -----------------
-    travels_df = travels_df.withColumn(
-        "Start_Gate", trim(regexp_replace("Start_Gate", "[^a-zA-Z0-9 ]", ""))
-    )
-    start_gate_analysis = travels_df.groupBy("Start_Gate").agg(
-        countDistinct("ID").alias("Start_Gate_Count")
-    )
-
-    # Print streaming start gate analysis DataFrame
-    start_gate_analysis_query = (
-        start_gate_analysis.writeStream.format("console").outputMode("complete").start()
-    )
-
-    # Wait for streaming queries to finish
-    travels_query.awaitTermination()
-    violation_query.awaitTermination()
-    violations_count_query.awaitTermination()
-    violations_count_by_type_query.awaitTermination()
-    start_gate_analysis_query.awaitTermination()
-
-except Exception as e:
-    LOGGER.error(f"An error occurred: {str(e)}")
+query.awaitTermination()
+spark.stop()
